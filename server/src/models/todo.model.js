c70d4aeecf0c4b3fd3b6b7e459a6fce0f6a2aba2
BeatrizRodriguezReglero <beatriz.rreglero@gmail.com>
2024-11-20 09:28:59 +0100

    loading

--- server/src/models/todo.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const TodoScheme = require('../schemes/todo.schemes');
-
-const TodoModel = mongoose.model('Todo', TodoScheme, 'tasks');
-
-module.exports = TodoModel;
--- server/src/schemes/todo.schemes.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const TodoScheme = mongoose.Schema({
-  _id: String,
-  name: String,
-  completed: Boolean
-});
-
-module.exports = TodoScheme;
